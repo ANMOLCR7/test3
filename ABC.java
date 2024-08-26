b85bac48405e00a4fa4d7d2c5b78da99eed13606
ANMOLCR7 <144582969+ANMOLCR7@users.noreply.github.com>
2024-08-26 11:33:41 +0530

    Add files via upload

---
+++ ABC.java
@@ -0,0 +1,28 @@
+
+import java.io.*;
+import java.util.Scanner;
+ 
+// Main class
+class ABC {
+ 
+    // Main Driver Method
+    public static void main(String[] args)
+    {
+       
+        int num = i;
+ 
+       
+        if (num % 2 == 0) {
+ 
+            // If remainder is zero then this number is even
+            System.out.println("Entered Number is Even");
+        }
+ 
+        else {
+ 
+            // If remainder is not zero then this number is
+            // odd
+            System.out.println("Entered Number is Odd");
+        }
+    }
+}
